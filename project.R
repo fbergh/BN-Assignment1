f31af3ff3e4094fb59fc7da5d32cb9713a43038f
Driessen, M.G.M. (Max) <m.g.driessen@student.ru.nl>
2019-11-14 10:00:14 +0100

    Added QQplots

--- project.R
+++ project.R
@@ -1,6 +1,12 @@
 # Import packages
 library(psych)          # For describe()
 
+# Function for creating QQ plots
+qq_plot = function(data) {
+  qqnorm(data)
+  qqline(data)
+}
+
 # Load data
 ff_orig = read.csv("forestfires.csv", header = TRUE)
 head(ff_orig)
@@ -19,12 +25,27 @@ ff_desc = describe(ff); ff_desc
 cor(ff)
 
 # Plot data to see if normally distributed
-hist(ff$FFMC)    # Skewed to right; not normal
-hist(ff$DMC)     # Skewed to left; normal-like?
-hist(ff$DC)      # Skewed to right; not normal
-hist(ff$ISI)     # Normal-like?
-hist(ff$temp)    # Normal
-hist(ff$RH)      # Skewed to left; normal-like?
-hist(ff$wind)    # Normal
-hist(ff$rain)    # Skewed to left; needs smaller bins for 0-2 range
-hist(ff$area)    # Skewed to left; needs smaller bins for range 0-200 range
+hist(ff$FFMC, breaks=50)    # Skewed to right; not normal
+hist(ff$DMC, breaks=50)     # Skewed to left; normal-like?
+hist(ff$DC, breaks=50)      # Skewed to right; not normal
+hist(ff$ISI, breaks=50)     # Normal-like?
+hist(ff$temp, breaks=50)    # Normal
+hist(ff$RH, breaks=50)      # Skewed to left; normal-like?
+hist(ff$wind, breaks=50)    # Normal
+hist(ff$rain, breaks=50)    # Skewed to left; needs smaller bins for 0-2 range
+hist(ff$area, breaks=50)    # Skewed to left; needs smaller bins for 0-200 range
+hist(log(ff$rain + 0.0001), breaks=50)
+hist(log(ff$area + 0.0001), breaks=50)
+
+# Create QQ plots of all attributes
+qq_plot(ff$FFMC)
+qq_plot(ff$DMC)
+qq_plot(ff$DC)
+qq_plot(ff$ISI)
+qq_plot(ff$temp)
+qq_plot(ff$RH)
+qq_plot(ff$wind)
+qq_plot(ff$rain)
+qq_plot(ff$area)
+qq_plot(log(ff$rain + 0.0001))
+qq_plot(log(ff$area + 0.0001))
