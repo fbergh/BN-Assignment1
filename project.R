ed6eae291f042ab35420a8f65db5b665b39e36ae
Bergh, F.J.P.N. van den (Freek) <f.vandenbergh@student.ru.nl>
2019-11-14 09:32:47 +0100

    Added plots to judge normality

--- project.R
+++ project.R
@@ -1,4 +1,5 @@
-library(psych)
+# Import packages
+library(psych)          # For describe()
 
 # Load data
 ff_orig = read.csv("forestfires.csv", header = TRUE)
@@ -13,3 +14,17 @@ ff$month = match(ff$month,month_abb_lower)
 ff$day = sapply(ff$day, function(day) day %in% weekend)
 head(ff)
 
+# Correlation matrix and basic statistics
+ff_desc = describe(ff); ff_desc
+cor(ff)
+
+# Plot data to see if normally distributed
+hist(ff$FFMC)    # Skewed to right; not normal
+hist(ff$DMC)     # Skewed to left; normal-like?
+hist(ff$DC)      # Skewed to right; not normal
+hist(ff$ISI)     # Normal-like?
+hist(ff$temp)    # Normal
+hist(ff$RH)      # Skewed to left; normal-like?
+hist(ff$wind)    # Normal
+hist(ff$rain)    # Skewed to left; needs smaller bins for 0-2 range
+hist(ff$area)    # Skewed to left; needs smaller bins for range 0-200 range
