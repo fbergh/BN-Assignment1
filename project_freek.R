0cfe34f5dd2b8c51863476b9ab06c1f8cb890a0d
Bergh, F.J.P.N. van den (Freek) <f.vandenbergh@student.ru.nl>
2019-11-20 12:21:24 +0100

    Created own project file project_freek.R with very simple analysis

---
+++ project_freek.R
@@ -0,0 +1,71 @@
+# Load packages
+library(dagitty)
+library(bnlearn)
+
+# Load data
+ff_orig = read.csv("forestfires.csv", header = TRUE)
+head(ff_orig)
+
+# Pre-process data
+month_abb_lower = lapply(month.abb,tolower)
+weekend = c("sat","sun")
+ff = ff_orig
+ff$X = NULL; ff$Y = NULL
+ff$month = match(ff$month,month_abb_lower)
+ff$day = sapply(ff$day, function(day) day %in% weekend)
+head(ff)
+
+# Make all columns double
+ff$month = as.double(ff$month); ff$day = as.double(ff$day); ff$area = as.double(ff$area); ff$RH = as.double(ff$RH)
+head(ff)
+
+# Create and plot DAG
+g = dagitty('
+            dag{
+                DC [pos="0.141,0.377"]
+                DMC [pos="-0.495,0.382"]
+                FFMC [pos="-1.160,0.387"]
+                ISI [pos="-1.132,0.919"]
+                RH [pos="-0.511,-0.482"]
+                area [pos="-0.464,1.349"]
+                day [pos="0.149,0.943"]
+                month [pos="-1.510,-0.902"]
+                rain [pos="0.141,-0.440"]
+                temp [pos="-1.168,-0.482"]
+                wind [pos="-1.804,-0.445"]
+                DC -> area
+                DMC -> area
+                day -> area
+                ISI -> area
+                FFMC -> ISI
+                wind -> ISI
+                temp -> FFMC
+                wind -> FFMC 
+                rain -> FFMC
+                RH -> FFMC
+                temp -> DMC
+                rain -> DMC
+                RH -> DMC
+                temp -> DC
+                rain -> DC
+                RH -> rain
+                temp -> RH
+                month -> temp
+                month -> wind
+            }
+            ')
+
+plot(g)
+impliedConditionalIndependencies(g)
+
+# Locally test network and show results with an absolute significant correlation greater than 0.1 (sorted on correlation magnitude)
+lt_out = localTests(g,ff)
+corr_lt_out = subset(lt_out, p.value<0.05 & abs(estimate)>0.1); corr_lt_out[order(abs(corr_lt_out$estimate)),]
+
+# Fit network to scaled (mean=0,std=1) data
+net <- model2network(toString(g,"bnlearn"))
+fit <- bn.fit( net, as.data.frame(scale(ff)) ); fit
+
+# Predict area given the day, DC, DMC and ISI attributes from our data and compute absolute error
+preds = predict(fit, node="area", data=ff[c("day","DC","DMC","ISI")])
+abs_error = abs(ff$area - preds); abs_error
