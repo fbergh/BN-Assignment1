da5b7359dad23361455a7b6da84ac292c3b7097f
Bergh, F.J.P.N. van den (Freek) <f.vandenbergh@student.ru.nl>
2019-11-28 11:18:39 +0100

    Added avg_wind; renamed month to avg_temp; made code up-to-date

--- project_freek.R
+++ project_freek.R
@@ -17,7 +17,6 @@ qq_plot = function(data) {
 month_abb_lower = lapply(month.abb,tolower)
 weekend = c("sat","sun")
 ff = ff_orig
-ff = ff[ff$area>0,]
 ff$X = NULL; ff$Y = NULL; ff$rain = NULL
 ff$month = match(ff$month,month_abb_lower)
 ff$day = sapply(ff$day, function(day) day %in% weekend)
@@ -28,20 +27,21 @@ head(ff)
 braganca_min_temp = c(0,1,3,5,8,12,14,14,12,8,4,1)
 braganca_max_temp = c(9,11,15,16,20,26,29,29,25,18,13,10)
 braganca_avg_temp = (braganca_min_temp + braganca_max_temp)/2
-ff$month = braganca_avg_temp[ff$month]
+ff$avg_temp = braganca_avg_temp[ff$month]
+braganca_avg_wind = c(5,6,6,6,5,5,5,5,4,4,5,4)
+ff$avg_wind = as.double(braganca_avg_wind[ff$month])
 head(ff)
 
 # Make all columns double
 ff$month = as.double(ff$month); ff$day = as.double(ff$day); ff$area = as.double(ff$area); ff$RH = as.double(ff$RH)
 head(ff)
 
-# ff$area = as.double(ff$area > 0)
-# qq_plot(ff$area)
-# # Add small value to zero values in area column such that the log-transform does not give "Inf" values
-# ff$area[ff$area < 0.5] = ff$area[ff$area < 0.5] + 0.5
-# ff$area = log(ff$area)
-# # Visualise QQ-plot of area to observe normality after addition of constant and log-transform
-# qq_plot(ff$area)
+qq_plot(ff$area)
+# Add small value to zero values in area column such that the log-transform does not give "Inf" values
+ff$area[ff$area < 0.5] = ff$area[ff$area < 0.5] + 0.5
+ff$area = log(ff$area)
+# Visualise QQ-plot of area to observe normality after addition of constant and log-transform
+qq_plot(ff$area)
 
 # Create train-test split (80/20)
 train_size = floor(0.8 * nrow(ff)); train_size
@@ -64,9 +64,10 @@ g = dagitty('
                 RH [pos="-0.511,-0.482"]
                 area [pos="-0.464,1.349"]
                 day [pos="0.149,0.943"]
-                month [pos="-1.510,-0.902"]
+                avg_temp [pos="-1.510,-0.902"]
                 temp [pos="-1.168,-0.482"]
                 wind [pos="-1.804,-0.445"]
+                avg_wind [pos="-2,-1"]
                 DC -> area
                 DMC -> area
                 day -> area
@@ -76,16 +77,15 @@ g = dagitty('
                 temp -> FFMC
                 wind -> FFMC
                 RH -> FFMC
-                month -> FFMC
+                avg_temp -> FFMC
                 temp -> DMC
                 RH -> DMC
-                month -> DMC
+                avg_temp -> DMC
                 temp -> DC
-                month -> DC
+                avg_temp -> DC
                 temp -> RH
-                month -> RH
-                month -> temp
-                month -> wind
+                avg_temp -> temp
+                avg_wind -> wind
             }
             ')
 
@@ -103,7 +103,6 @@ fit <- bn.fit( net, as.data.frame(ff_train) ); fit
 
 # Predict area given the test data and compute absolute error
 preds = predict(fit, node="area", data=ff_test)
-# preds = as.double(preds > 0.5)
 abs_error = abs(ff_test$area - preds); abs_error
 
 # Show ground truth and predictions
