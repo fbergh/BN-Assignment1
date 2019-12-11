b9843cafb3a1fd2a35c63417ae6a48d23f28f219
Bergh, F.J.P.N. van den (Freek) <f.vandenbergh@student.ru.nl>
2019-12-11 14:47:48 +0100

    Renamed day attribute to weekend; beautified plots; added code for saving plots; removed unnecessary code

--- project_freek.R
+++ project_freek.R
@@ -21,7 +21,7 @@ ff = ff_orig
 ff$X = NULL; ff$Y = NULL; ff$rain = NULL
 # Convert month variable to numbers and day variable to boolean of weekend
 ff$month = match(ff$month,month_abb_lower)
-ff$day = sapply(ff$day, function(day) day %in% weekend)
+ff$weekend = as.double(sapply(ff$day, function(day) day %in% weekend))
 head(ff)
 
 # Replace month numbers with average temperatures per month of Bragança, a place nearby Monteshino Park
@@ -35,7 +35,7 @@ ff$avg_wind = as.double(braganca_avg_wind[ff$month])
 head(ff)
 
 # Make all columns double
-ff$month = NULL; ff$day = as.double(ff$day); ff$area = as.double(ff$area); ff$RH = as.double(ff$RH)
+ff$month = NULL; ff$day = NULL; ff$area = as.double(ff$area); ff$RH = as.double(ff$RH)
 head(ff)
 
 qq_plot(ff$area)
@@ -43,6 +43,8 @@ qq_plot(ff$area)
 ff$area = log(ff$area + 1.0)
 # Visualise QQ-plot of area to observe normality after addition of constant and log-transform
 qq_plot(ff$area)
+dev.off()
+describe(ff)
 
 # Create and plot DAG
 g = dagitty('
@@ -53,14 +55,14 @@ g = dagitty('
                 ISI [pos="-1.132,0.919"]
                 RH [pos="-0.511,-0.482"]
                 area [pos="-0.464,1.349"]
-                day [pos="0.149,0.943"]
+                weekend [pos="0.149,0.943"]
                 avg_temp [pos="-1,-0.902"]
                 temp [pos="-1.168,-0.482"]
                 wind [pos="-1.804,-0.445"]
                 avg_wind [pos="-2,-1"]
                 DC -> area
                 DMC -> area
-                day -> area
+                weekend -> area
                 ISI -> area
                 FFMC -> ISI
                 wind -> ISI
@@ -92,25 +94,41 @@ net <- model2network(toString(g,"bnlearn"))
 fit <- bn.fit( net, as.data.frame(ff) ); fit
 
 # Predict area given the test data and compute absolute error
-preds = predict(fit, node="area", data=ff)
+preds_area = predict(fit, node="area", data=ff)
 abs_error = abs(ff$area - preds); abs_error
 
+# To en/disable saving plots comment out/in the lines with "png()" and "dev.off()"
 # Show ground truth and predictions
-ff$area; preds
-plot(preds, ff$area); abline(coef = c(0,1))
-cor.test(preds, ff$area)
+# png(filename="img/area-preds-plot.png")
+plot(preds_area, ff$area, ann=FALSE); abline(coef = c(0,1), c="red"); title(main="Predictions of area plotted as a function of the ground truth"); title(xlab="Ground Truth of area"); title(ylab="Predictions of area")
+# dev.off()
+cor.test(preds_area, ff$area)
 
-# Predict FFMC, DMC, DC, and ISI
-preds_FFMC = predict(fit, node="FFMC", data=ff[c("wind","temp","avg_temp","RH")]); plot(preds_FFMC, ff$FFMC); abline(coef = c(0,1))
-# Plot FFMC without outlier
-plot(preds_FFMC[ff$FFMC>=80], ff$FFMC[ff$FFMC>=80]); abline(coef = c(0,1))
+# Predict FFMC, DMC, DC, and ISI and plot them
+preds_FFMC = predict(fit, node="FFMC", data=ff[c("wind","temp","avg_temp","RH")])
+# png(filename="img/FFMC-preds-plot.png")
+plot(preds_FFMC, ff$FFMC, ann=FALSE); abline(coef = c(0,1), c="red"); title(main="Predictions of FFMC plotted as a function of the ground truth"); title(xlab="Ground Truth of FFMC"); title(ylab="Predictions of FFMC")
+# dev.off()
 cor.test(preds_FFMC,ff$FFMC)
+t.test(preds_FFMC, ff$FFMC, paired=TRUE)
 
-preds_DMC = predict(fit, node="DMC", data=ff[c("avg_temp","temp","RH")]); plot(preds_DMC, ff$DMC); abline(coef = c(0,1))
+preds_DMC = predict(fit, node="DMC", data=ff[c("avg_temp","temp","RH")])
+# png(filename="img/DMC-preds-plot.png")
+plot(preds_DMC, ff$DMC, ann=FALSE); abline(coef = c(0,1), c="red"); title(main="Predictions of DMC plotted as a function of the ground truth"); title(xlab="Ground Truth of DMC"); title(ylab="Predictions of DMC")
+# dev.off()
 cor.test(preds_DMC,ff$DMC)
+t.test(preds_DMC, ff$DMC, paired=TRUE)
 
-preds_DC = predict(fit, node="DC", data=ff[c("avg_temp","temp")]); plot(preds_DC, ff$DC); abline(coef = c(0,1))
+preds_DC = predict(fit, node="DC", data=ff[c("avg_temp","temp")])
+# png(filename="img/DC-preds-plot.png")
+plot(preds_DC, ff$DC, ann=FALSE); abline(coef = c(0,1), c="red"); title(main="Predictions of DC plotted as a function of the ground truth"); title(xlab="Ground Truth of DC"); title(ylab="Predictions of DC")
+# dev.off()
 cor.test(preds_DC,ff$DC)
+t.test(preds_DC, ff$DC, paired=TRUE)
 
-preds_ISI = predict(fit, node="ISI", data=ff[c("wind","FFMC")]); plot(preds_ISI, ff$ISI); abline(coef = c(0,1))
-cor.test(preds_ISI,ff$ISI)
+preds_ISI = predict(fit, node="ISI", data=ff[c("wind","FFMC")])
+# png(filename="img/ISI-preds-plot.png")
+plot(preds_ISI, ff$ISI, ann=FALSE); abline(coef = c(0,1), c="red"); title(main="Predictions of ISI plotted as a function of the ground truth"); title(xlab="Ground Truth of ISI"); title(ylab="Predictions of ISI")
+# dev.off()
+cor.test(preds_ISI, ff$ISI)
+t.test(preds_ISI, ff$ISI, paired=TRUE)
